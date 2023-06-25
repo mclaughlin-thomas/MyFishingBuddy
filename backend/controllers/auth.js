4e0aefb9f8a9e12bae7f7c20a552daf931344755
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-06-25 17:57:12 -0400

    login cookies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,12 @@
 import bcrpytjs from 'bcryptjs';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
-export const register = async(req, res) =>{
+export const register = async(req, res) => {
     if(!req.body.name || !req.body.email || !req.body.password){
         return res.json('required field email, name, or password');
-    } // make individual ifs!
+    } 
+    // make individual ifs!
 
     try{
         const salter = await bcrpytjs.genSalt(10);
@@ -24,6 +26,34 @@ export const register = async(req, res) =>{
     }
 };
 
-export const login = () =>{
-
+export const login = async(req, res) =>{
+    if(!req.body.email || !req.body.password){
+        return res.json('required fields: name, password');
+    }
+    try{
+        const user = await User.findOne({email: req.body.email}).select(
+            'name email password',
+        );
+        if(!user){
+            return res.status(404).json('No user found');
+        }
+        const correctPass = await bcrpytjs.compare(req.body.password, user.password);
+        if (!correctPass){
+            return res.json('password is incorrect');
+        }
+        const payload = {
+            id: user._id,
+            name: user.name
+        }
+        const token = jwt.sign(payload, process.env.JWT_SECRET,{
+            expiresIn: '1d'
+        })
+        return res.cookie('access_token', token,{
+            httpOnly: true,
+        }).status(200)
+        .json({message: "Login Success"});
+    }catch(err){
+        console.log(err);
+        return res.json('server error');
+    }
 };
