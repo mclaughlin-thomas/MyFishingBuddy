519baf09de21af181d912b9189c731f3d0bdc9bd
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-06-27 14:25:34 -0400

    Completed fish routes

--- backend/controllers/fish.js
+++ backend/controllers/fish.js
@@ -52,3 +52,20 @@ export const updateFish = async (req,res,next)=> {
         return next(err);
     }
 }
+
+export const deleteFish = async (req,res,next)=>{
+    try{
+        const task = await Fish.findById(req.params.fishId).exec();
+        if(!task){
+            return next(createError({status: 404, message: "No Fish were found!"}));
+        }
+        if(task.user.toString() !==req.user.id){
+            return next(createError({status: 404, message: "That not your Fishy!"}));
+        }
+
+        await Fish.findByIdAndDelete(req.params.fishId);
+        return res.status(200).json('Task deleted successfully!');
+    }catch(err){
+        return next(err);
+    }
+}
--- backend/routes/fish.js
+++ backend/routes/fish.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, getAllFish, getCurrentUsersFish, updateFish } from '../controllers/fish.js';
+import { createTask, getAllFish, getCurrentUsersFish, updateFish, deleteFish } from '../controllers/fish.js';
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post('/', createTask);
 router.get('/all',getAllFish);
 router.get('/myFish', getCurrentUsersFish);
 router.put('/:fishId', updateFish);
+router.delete('/:fishId', deleteFish);
 
 
 export default router;
