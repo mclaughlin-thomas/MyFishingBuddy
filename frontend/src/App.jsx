508e6ef163a9a35cd45745a11bd4a7944806cfe7
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-07-04 15:24:30 -0400

    Created react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
+import {Routes, Route} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
 import React from 'react';
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
+import Auth from './pages/Auth';
 
 export default function App() {
   return (
     <>
-        <Toaster position='top-right'
+        <Toaster
+         position='top-right'
          toastOptions={{
             style: {
-                fontSize: '1.8rem'
+              fontSize: '1.8rem'
             }
         }}
         >Hi
         </Toaster>
         
-        <Routes></Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/edit-profile' element={<EditProfile />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
     </>
   )
 }
