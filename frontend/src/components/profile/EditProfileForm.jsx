402341cb68738fa8c0d60b7f64394ea42580e130
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2023-07-16 00:21:30 -0400

    Fixed auth issue for logged in users

--- frontend/src/components/profile/EditProfileForm.jsx
+++ frontend/src/components/profile/EditProfileForm.jsx
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import classes from './EditProfileForm.module.scss';
 import {BsArrowLeftShort} from 'react-icons/bs';
+import axios from 'axios';
+import {toast} from 'react-hot-toast';
 
 function EditProfileForm() {
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+    });
+
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const { data } = await axios.get('/api/users/me');
+                    setUser(data);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        )();
+    }, []);
+
+    const updateUserInfo = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const updateProfile = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.put('/api/users/me', user);
+            toast.success('Profile updated successfully');
+            setUser(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
   return (
     <div>
         <Link to="/" className={classes.backBtn}>
             <BsArrowLeftShort/>
             Home
         </Link>
+        <div>
+            <h1>Edit Profile</h1>
+            <form className={classes.editForm} onSubmit={updateProfile}>
+                <label htmlFor="name">
+                    Full Name
+                    <input type="text" name="name" placeholder="Full Name"
+                    required value={user.name} onChange={updateUserInfo}/>
+                </label>
+                <label htmlFor="email">
+                    Email
+                    <input type="email" name="email" placeholder="Email"
+                    required value={user.email} onChange={updateUserInfo}/>
+                </label>
+                <button type='submit'>Save</button>
+            </form>
+        </div>
     </div>
   );
 }
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register'
 import Layout from '../components/Layout';
 import classes from './Auth.module.scss';
+import userAuth from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 function Auth() {
+  
+  const {auth} = userAuth();
+  const nav = useNavigate();
+
+  useEffect(()=>{
+    if (auth){
+      nav('/');
+    }
+  },[auth, nav])
+
   return (
     <Layout>
       <div className={classes.form_container}>
